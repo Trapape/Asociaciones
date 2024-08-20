ed14d813372a85f6584807a5825e7b9808889717
Rcarlohh <rangel.crlos@gmail.com>
2024-08-19 20:49:44 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// App.jsx
-
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layouts/LayoutComponent.jsx';
 import HomeScreen from './screens/HomeScreen.jsx';
@@ -9,35 +7,32 @@ import ServiceScreen from './screens/ServiceScreen.jsx';
 import ForumScreen from './screens/ForumScreen.jsx';
 import ContactScreen from './screens/ContactScreen.jsx';
 import LoginModalComponent from './components/ForumComponents/LoginModalComponent/LoginModalComponent.jsx'; 
-import { UserContext } from '../backend/config/UserContext.jsx'; // Importa el contexto de usuario
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen.jsx';
+import SettingsScreen from './screens/SettingsScreen/SettingsScreen.jsx';
+import BlogScrenn from './screens/BlogScreen/BlogScreen.jsx';
+import { UserContext } from '../backend/config/UserContext.jsx'; 
+
 
 function App() {
-  const [user, setUser] = useState(null); // Estado de sesión de usuario
-
-  useEffect(() => {
-    // Verificar la sesión al cargar la aplicación
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
+  const { user } = useContext(UserContext); // Utiliza el contexto de usuario
 
   return (
     <Router>
-      <UserContext.Provider value={{ user, setUser }}>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/About" element={<AboutScreen />} />
-            <Route path="/Service" element={<ServiceScreen />} />
-            <Route path="/Forum" element={<ForumScreen />} />
-            <Route path="/Contact" element={<ContactScreen />} />
-          </Routes>
-        </Layout>
-        {/* Mostrar el modal de inicio de sesión solo cuando no hay sesión activa */}
-        {!user && <LoginModalComponent />}
-      </UserContext.Provider>
+      <Routes>
+        {/* Rutas que usan el layout */}
+        <Route path="/" element={<Layout><HomeScreen /></Layout>} />
+        <Route path="/About" element={<Layout><AboutScreen /></Layout>} />
+        <Route path="/Service" element={<Layout><ServiceScreen /></Layout>} />
+        <Route path="/Forum" element={<Layout><ForumScreen /></Layout>} />
+        <Route path="/Contact" element={<Layout><ContactScreen /></Layout>} />
+
+        {/* Rutas que no usan el layout */}
+        <Route path="/Profile" element={<ProfileScreen />} />
+        <Route path="/Settings" element={<SettingsScreen />} />
+        <Route path="/Blog" element={<BlogScrenn />} />
+        <Route path="/Blog/:eventId" element={<BlogScrenn />} />
+      </Routes>
+      {!user && <LoginModalComponent />}
     </Router>
   );
 }
